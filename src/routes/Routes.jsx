e879e1bc6ce254e45c1a6136b1586367416762de
Ridwan4D <ridwanul161@gmail.com>
2024-05-10 02:01:27 +0600

    register page created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout/>,
       errorElement: <ErrorPage/>,
+      children:[
+        {
+          path: "/login",
+          element: <Login/>
+        },
+        {
+          path: "/register",
+          element: <Register/>
+        }
+      ]
     },
   ]);
 
